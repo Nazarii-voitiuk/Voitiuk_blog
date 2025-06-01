a41729385a657e6eedd2e25ad469684117433bed
Nazar Voitiuk <nazarii.voitiuk@oa.edu.ua>
2025-06-01 05:24:54 +0000

    Лабораторна 2. Створення міграція і заповнення даних в таблицях

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,11 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        $this->call(UsersTableSeeder::class);
+        $this->call(BlogCategoriesTableSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\BlogPost::factory(100)->create();
     }
 }
