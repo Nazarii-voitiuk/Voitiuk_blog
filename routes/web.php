ac015f5107f8cb14854a98830e64d08780b371c3
Nazar Voitiuk <nazarii.voitiuk@oa.edu.ua>
2025-06-07 21:51:52 +0000

    Лабораторна 7. Вивід статей блога

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
 use App\Http\Controllers\Blog\PostController;
+use App\Http\Controllers\Blog\Admin\PostController as AdminPostController;
 use App\Http\Controllers\Blog\Admin\CategoryController;
 
 Route::get('/', function () {
@@ -29,14 +30,16 @@ Route::group([
 });
 
 
-//Адмінка
+// Адмінка
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
     'prefix' => 'admin/blog',
 ];
 Route::group($groupData, function () {
-    $methods = ['index','edit','store','update','create'];
+    Route::resource('posts', AdminPostController::class)
+        ->except(['show'])
+        ->names('blog.admin.posts');
     Route::resource('categories', CategoryController::class)
-        ->only($methods)
+        ->only(['index','edit','store','update','create'])
         ->names('blog.admin.categories');
 });
